8e33083578b304839ed41ab97eb7a1d9925f3683
Christian Pappert <chrpap@microsoft.com>
2020-12-26 14:44:36 -0800

    fixing .runsettings not found

--- testsf_on_linux.sh
+++ testsf_on_linux.sh
@@ -79,5 +79,5 @@ dotnet build Tests/Fluent.Tests/Fluent.Tests.csproj -f $netcore20
 
 echo Running Fluent Tests
 cd $rootdir/Tests/Fluent.Tests
-dotnet test Fluent.Tests.csproj -f $netcore20 --filter ClassName=Fluent.Tests.ServiceFabric -s ~/Tests/Fluent.Tests/ServiceFabric.runsettings
+dotnet test Fluent.Tests.csproj -f $netcore20 --no-build --filter ClassName=Fluent.Tests.ServiceFabric -s ServiceFabric.runsettings
 
